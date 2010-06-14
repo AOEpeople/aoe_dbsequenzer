769e46fc5fad35915b868e098cc8629a75a35495
Oliver Hader <oliver.hader@aoe.com>
2010-06-14 17:46:10 +0000

    Fixed bug: Incomplete XCLASS definitions

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,10 @@ require_once(t3lib_extMgm::extPath($_EXTKEY).'Classes/TYPO3Service.php');
 
 if (!isset($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_db.php'])) {
 	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_db.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/class.ux_t3lib_db.php';
-}
-else {
+} else {
+	// Backwards compatibility to older t3p_scalable versions:
+	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3p_scalable/typo3versions/4.2.FF/class.ux_t3lib_db.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/class.ux_ux_t3lib_db.php';
 	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3p_scalable/typo3versions/4.3.FF/class.ux_t3lib_db.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/class.ux_ux_t3lib_db.php';
+	// State of t3p_scalable version 1.4.0++
+	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3p_scalable/class.ux_t3lib_db.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/class.ux_ux_t3lib_db.php';
 }
