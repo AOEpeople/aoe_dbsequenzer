25f5f85a72ab3f3dab2987fc2a3b04811c162868
Jürgen Kussmann <juergen.kussmann@aoe.com>
2012-05-29 13:12:18 +0000

    Refactored positioning of table-fields in TCA

--- ext_tables.php
+++ ext_tables.php
@@ -12,13 +12,41 @@ if (TYPO3_MODE == 'BE') {
 	$column ['tx_aoe_dbsquenzer_protectoverwrite_till'] = array ();
 	$column ['tx_aoe_dbsquenzer_protectoverwrite_till'] ['label'] = 'LLL:EXT:aoe_dbsequenzer/Resources/Private/Language/locallang_db.xml:protectoverwrite_till';
 	$column ['tx_aoe_dbsquenzer_protectoverwrite_till'] ['config'] = array ();
-	
 	$column ['tx_aoe_dbsquenzer_protectoverwrite_till'] ['config'] ['type'] = 'user';
 	$column ['tx_aoe_dbsquenzer_protectoverwrite_till'] ['config'] ['userFunc'] = 'Tx_AoeDbsequenzer_OverwriteProtectionService->renderInput';
 	$column ['tx_aoe_dbsquenzer_protectoverwrite_till'] ['config'] ['eval'] = 'datetime';
+
+	$columnsConfig = 'tx_aoe_dbsquenzer_protectoverwrite,tx_aoe_dbsquenzer_protectoverwrite_till';
+	global $TCA;
+
 	foreach ( $tables as $table ) {
+		// add columnsConfig at END of TCA-configuration
+		t3lib_div::loadTCA( $table );
 		t3lib_extMgm::addTCAcolumns ( $table, $column, 1 );
-		t3lib_extMgm::addToAllTCAtypes ( $table, 'tx_aoe_dbsquenzer_protectoverwrite,tx_aoe_dbsquenzer_protectoverwrite_till' );
+		t3lib_extMgm::addToAllTCAtypes ( $table, $columnsConfig );
+
+		// move columnsConfig from END of TCA-configuration to BEGIN of TCA-configuration
+		if(is_array($TCA[$table]['types'])) {
+			foreach($TCA[$table]['types'] as &$tableTypeConfig) {
+				if(array_key_exists('showitem', $tableTypeConfig) && stristr($tableTypeConfig['showitem'], $columnsConfig)) {
+					$showItems = &$tableTypeConfig['showitem'];
+
+					// 1. delete columnsConfig at END of TCA-configuration
+					$showItems = str_replace(','.$columnsConfig, '', $showItems);
+
+					// 2. add columnsConfig at BEGIN of TCA-configuration
+					if(preg_match('/^--div--/i', $showItems)) {
+						// first entry is an tab
+						$firstColumnEntry = substr($showItems, 0, stripos($showItems, ',') + 1);
+						$showItems = str_replace($firstColumnEntry, '', $showItems);
+						$showItems = $firstColumnEntry . $columnsConfig . ',' . $showItems;
+					} else {
+						// first entry is no tab
+						$showItems = $columnsConfig . ',' . $showItems;
+					}
+				}
+			}
+		}
 	}
 	
 	$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:aoe_dbsequenzer/Classes/OverwriteProtectionService.php:Tx_AoeDbsequenzer_OverwriteProtectionService';
