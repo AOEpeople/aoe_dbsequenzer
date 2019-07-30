b4c249df1f5b3c2c160f01ad3b776a2306bd7c8e
Thomas Schenk <Thomas.Schenk@aoe.com>
2019-07-30 08:33:41 +0200

    [TASK] updates version & structure in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,33 +3,33 @@
  * Extension Manager/Repository config file for ext "aoe_dbsequenzer".
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'AOE Database Sequenzer',
-	'description' => 'With this extension you can ensure different unique keys for the configured tables',
-	'category' => 'misc',
-	'author' => 'Dev-Team AOE',
-	'author_company' => 'AOE GmbH',
-	'author_email' => 'dev@aoe.com',
-	'shy' => '',
-	'dependencies' => 'extbase',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '3.0.0',
-	'constraints' => array(
-		'depends' => array(
-			'extbase' => '',
-            'typo3' => '7.6.0-7.6.99',
-		),
-		'conflicts' => array(),
-		'suggests' => array(),
-	),
-	'_md5_values_when_last_written' => '',
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'AOE Database Sequenzer',
+    'description' => 'With this extension you can ensure different unique keys for the configured tables',
+    'category' => 'misc',
+    'author' => 'Dev-Team AOE',
+    'author_company' => 'AOE GmbH',
+    'author_email' => 'dev@aoe.com',
+    'shy' => '',
+    'dependencies' => 'extbase',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '3.0.0',
+    'constraints' => [
+        'depends' => [
+            'extbase' => '',
+            'typo3' => '8.7.0-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    '_md5_values_when_last_written' => '',
+];
