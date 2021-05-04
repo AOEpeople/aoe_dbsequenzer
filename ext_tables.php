9227fb84f271eee2748ec2c372521b47c1a38c97
Thomas Schenk <Thomas.Schenk@aoe.com>
2021-05-04 13:41:32 +0200

    [CLEANUP] removes feature overwrite protection

--- ext_tables.php
+++ ext_tables.php
@@ -2,13 +2,3 @@
 if (! defined ( 'TYPO3_MODE' )) {
     die ( 'Access denied.' );
 }
-
-if (TYPO3_MODE == 'BE') {
-	$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
-        \Aoe\AoeDbSequenzer\Service\OverwriteProtectionService::class;
-
-	$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] =
-        \Aoe\AoeDbSequenzer\Service\OverwriteProtectionService::class;
-
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_aoedbsequenzer_domain_model_overwriteprotection');
-}
--- ext_tables.sql
+++ ext_tables.sql
@@ -9,20 +9,3 @@ CREATE TABLE tx_aoedbsequenzer_sequenz (
 	changed int(11) DEFAULT '0' NOT NULL,
 	UNIQUE KEY tablename (tablename)
 ) ENGINE=InnoDB;
-#
-# Table structure for table 'tx_aoedbsequenzer_overwriteprotection'
-#
-CREATE TABLE tx_aoedbsequenzer_domain_model_overwriteprotection (
-    uid int(11) NOT NULL auto_increment,
-    pid int(11) DEFAULT '0' NOT NULL,
-    tstamp int(11) DEFAULT '0' NOT NULL,
-    sorting int(10) DEFAULT '0' NOT NULL,
-    crdate int(11) DEFAULT '0' NOT NULL,
-    cruser_id int(11) DEFAULT '0' NOT NULL,
-    protected_uid int(11) DEFAULT '0' NOT NULL,
-    protected_tablename  varchar(100) DEFAULT '' NOT NULL,
-    protected_time int(11) DEFAULT '0' NOT NULL,
-    protected_mode int(11) DEFAULT '0' NOT NULL,
-    PRIMARY KEY (uid),
-    KEY parent (pid)
-) ENGINE=InnoDB;
