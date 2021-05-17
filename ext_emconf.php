c6d1ce8ac6a52048f5bd74468c7ab1e6c63fa9ed
Thomas Layh <thomas.layh@aoe.com>
2021-05-17 09:40:03 +0200

    [RELEASE] version 5.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,11 +22,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '5.0.0',
+    'version' => '5.1.0',
     'constraints' => [
         'depends' => [
             'extbase' => '',
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
