9adb1a52b9038f4e297673f8ab04ceea14a81dc2
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2021-07-09 10:49:24 +0200

    [TASK] Adjust default value for tinyint to be strictmode compatible

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,9 +3,9 @@
 #
 CREATE TABLE tx_aoedbsequenzer_sequenz (
 	tablename varchar(100) DEFAULT '' NOT NULL,
-	current int(30) DEFAULT '0' NOT NULL,
-	offset int(30) DEFAULT '1' NOT NULL,
-	timestamp int(30) DEFAULT '0' NOT NULL,
-	changed int(11) DEFAULT '0' NOT NULL,
+	current int(30) DEFAULT 0 NOT NULL,
+	offset int(30) DEFAULT 1 NOT NULL,
+	timestamp int(30) DEFAULT 0 NOT NULL,
+	changed int(11) DEFAULT 0 NOT NULL,
 	UNIQUE KEY tablename (tablename)
 ) ENGINE=InnoDB;
